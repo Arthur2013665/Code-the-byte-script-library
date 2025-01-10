e8c07af83769ba5ceaea82c689b24a12503f6053
Arthur2013665 <139013194+Arthur2013665@users.noreply.github.com>
2025-01-10 14:49:26 +0200

    Update Script starter.lua

--- Script starter.lua
+++ Script starter.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Arthur2013665/MY-ROBOX-SCRIPT/refs/heads/main/Script.lua?token=GHSAT0AAAAAAC4P3RGKGTGLSBY5TDC2K7MCZ35R7BA"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Arthur2013665/Code-the-byte-script-library/refs/heads/main/Script.lua?token=GHSAT0AAAAAAC4P3RGLFY47O47FOGGFPNZOZ4BC55Q"))()
